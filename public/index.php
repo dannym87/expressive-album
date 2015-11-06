9a245942270b700991d86f4b215872e939681d82
Dan Messenger <dannym87@gmail.com>
2015-11-06 13:47:47 +0000

    Tidying up repository fetchAll method

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Europe/London');
 /**
  * Display all errors when APPLICATION_ENV is development.
  */
-if ($_SERVER['APPLICATION_ENV'] == 'development') {
+if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 }
